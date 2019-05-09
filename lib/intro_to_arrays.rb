467c397a279262a706ca28d27ac8822c01e7d0ce
Pablo Gonzalez <pabgon0@gmail.com>
2019-05-09 01:56:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -4,7 +4,20 @@ def instantiate_new_array
   array2 = []
   array2
 end
+
 def array_with_two_elements
   array1 = ["1", "2"]
   array1
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
 end
